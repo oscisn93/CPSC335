406ab6411745c8ae6f201b7c1b10e6cb88c198e1
Oscar <ocisneros1@csu.fullerton.edu>
2023-03-19 23:33:15 -0700

    added comments

--- project_2/algorithm_2/main.cpp
+++ project_2/algorithm_2/main.cpp
@@ -3,25 +3,36 @@
 #include <string>
 
 std::string encode(std::string s) {
+// use a stringstream to prevent memory resizing 
+// involved in string concatenations
   std::stringstream ss;
+  // reference to the current character;
   char cur;
+  // total of the current. always >= 1
   int count = 1;
   for (int i = 0; i < s.size(); i++) {
+    // cur is the current character
     cur = s[i];
+    // advance until the next character no longer matches cur
     while (i < s.size() - 1 && s[i+1] == cur) {
       count++;
       i++;
     }
+    // if count > 1, there are multiple of the current string so we add that value
     if (count > 1) {
       ss << count;
     }
+    // then we add the character
     ss << cur;
+    // reset the count
     count = 1;
   }
+  // retrun a string with the contents of the stringstream
   return ss.str();
 }
 
 int main() {
+  // call encode on each string and print result
   for (std::string line; std::getline(std::cin, line);) {
     std::cout << encode(line) << std::endl;
   }
--- project_2/algorithm_3/main.cpp
+++ project_2/algorithm_3/main.cpp
@@ -1,33 +1,29 @@
+#include <algorithm>
 #include <cctype>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <sstream>
 
-void print_vec(std::vector<int> &v){
-  std::stringstream ss;
-  ss << "[";
-  for (int n: v) {
-    ss << n;
-    ss << ",";
-  }
-  std::string s = ss.str();
-  s[s.size() - 1] = ']';
-  std::cout << s << std::endl;
-}
-
-std::vector<std::vector<int>> parse() {
+std::vector<std::vector<int>> input_parser() {
+  // we use a flag to determine when it's time to
+  // stop parsing the current array
   bool in_array = false;
-  std::vector<std::vector<int>> nums;
+  std::vector<std::vector<int>> _2d_array;
+  // read directly from standard input
   for (std::string line; std::getline(std::cin, line);) {
+    // set in array to true to parse the next array
     if (line[0] == 'A') {
       in_array = true;
       std::vector<int> v;
-      nums.push_back(v);
+      _2d_array.push_back(v);
     } else if (line.size() == 0) {
+      // set to false when the line is empty 
+      // so we can create a new empty array
       in_array = false;
     }
+    // parse the current array- we're just 
+    // reading in numbers until the line ends
     if (in_array) {
       int i = 0;
       // advance past first bracket
@@ -45,46 +41,71 @@ std::vector<std::vector<int>> parse() {
             end = i;
           i++;
         }
+        // advance the pointer past the last character
         i++;
+        // add the full number to the current array
         if (start > -1 && end > -1)
-          nums[nums.size() - 1].push_back(
+          _2d_array[_2d_array.size() - 1].push_back(
               std::stoi(line.substr(start, end - start + 1)));
       }
     }
   }
-  return nums;
+  return _2d_array;
 }
 
-int partition(int i, int j, std::vector<int> &v) {
-  int pivot_element = v[i]; 
+
+
+static int partition(int i, int j, std::vector<int> &v) {
+  // set the pivot to the leftmost element
+  int pivot_element = v[i];
   int start = i;
-  int end = j ;
+  int end = j;
   while (true) {
-    while (v[end] > pivot_element) end--;
-    while (v[start] < pivot_element) start++;
-    if (start < end){
+    // advance backaeards past all elements larger than the pivot
+    while (v[end] > pivot_element)
+      end--;
+      // advance forward past all elements smaller than the pivot
+    while (v[start] < pivot_element)
+      start++;
+    // if there are still numbers in between, 
+    // swap and repeat, until the subpartition is sorted
+    if (start < end) {
       std::iter_swap(v.begin() + start, v.begin() + end);
       start++;
       end--;
-    } else return start;
+      // otherwise retrun the new pivot
+    } else
+      return start;
   }
 }
 
-
 void quick_sort(int i, int j, std::vector<int> &v) {
-  if (i < j){
+  if (i < j) {
+    // find a partition index and sort both halfs until i >= j
     int partition_index = partition(i, j, v);
     quick_sort(i, partition_index, v);
     quick_sort(partition_index + 1, j, v);
   }
 }
+// utility function to easily print vectors
+void print_vec(std::vector<int> &v) {
+  std::stringstream ss;
+  ss << "[";
+  for (int n : v) {
+    ss << n;
+    ss << ",";
+  }
+  std::string s = ss.str();
+  s[s.size() - 1] = ']';
+  std::cout << s << std::endl;
+}
 
 int main() {
-  std::vector<std::vector<int>> p = parse();
-  for (std::vector<int> parsed_input: p){
-    quick_sort(0, parsed_input.size() - 1, parsed_input);
-    print_vec(parsed_input);
+  // parse, sort, print
+  auto p = input_parser();
+  for (auto input : p) {
+    quick_sort(0, input.size() - 1, input);
+    print_vec(input);
   }
   return 0;
 }
-
