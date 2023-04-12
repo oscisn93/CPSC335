522fb998e6716a464516f9b3608b2a66a6212f6b
Oscar Cisneros <41174627+oscisn93@users.noreply.github.com>
2023-04-12 02:52:46 +0000

    added build script

---
+++ project_3/build.sh
@@ -0,0 +1,2 @@
+clang++ dynamic_programming/main.cpp -o dynamic_programming/main
+clang++ exhaustive_search/main.cpp -o exhaustive_search/main
--- project_3/dynamic_programming/main.cpp
+++ project_3/dynamic_programming/main.cpp
@@ -5,58 +5,93 @@
 #include <string>
 #include <vector>
 #include <chrono>
-
-auto parse() {
+// parse the input from stdin
+// from input, create 2d char vector
+// that represents the soccer field
+auto parse()
+{
+  // the grid will be a 2d char vector
   std::vector<std::vector<char>> grid;
-  for (std::string line; std::getline(std::cin, line);) {
+  // process each stdin line independently
+  for (std::string line; std::getline(std::cin, line);)
+  {
+    // create a new vector for each line
+    // populate it with the characters
     std::vector<char> row;
-    for (char c : line) {
+    for (char c : line)
+    {
       row.push_back(c);
     }
+    // push the row onto the grid and repeat
     grid.push_back(row);
   }
   return grid;
 }
-
-int soccer_opponent_aviodance(std::vector<std::vector<char>> grid) {
+// dynamic programming approach to the problem
+int soccer_opponent_aviodance(std::vector<std::vector<char>> grid)
+{
   // check if the first square is blocked
-  // if so no paths exist 
+  // if so no paths exist
   if (grid[0][0] == 'X')
     return 0;
-   //  use a 2-D array to track the number of paths at each square
+  //  use a 2-D array to track the number of paths at each square
   int matrix[grid.size()][grid[0].size()];
   //  fill the matrix with 0s
-  for (int i = 0; i < grid.size(); i++) {
-    for (int j = 0; j < grid[0].size(); j++) {
+  for (int i = 0; i < grid.size(); i++)
+  {
+    for (int j = 0; j < grid[0].size(); j++)
+    {
       matrix[i][j] = 0;
     }
   }
   //  first one is a path since it's not X
   matrix[0][0] = 1;
-
-  for (int i = 0; i < grid.size(); i++) {
-    for (int j = 0; j < grid[0].size(); j++) {
+  // iterate through the grid and calculate paths per square
+  for (int i = 0; i < grid.size(); i++)
+  {
+    for (int j = 0; j < grid[0].size(); j++)
+    {
       int above = 0, left = 0;
-      if (grid[i][j] == 'X') {
+      // if the current square has an X
+      // set the matrix value to 0
+      // and continue iterating
+      if (grid[i][j] == 'X')
+      {
         matrix[i][j] = 0;
         continue;
       }
+      // case: the grid quare contains a '.'
+      // if a path exists above and the square
+      // is not on the first row
       if (i > 0 && grid[i - 1][j] == '.')
+        // set above value to greater than 0
         above = matrix[i - 1][j];
+      // repeat for the left value
       if (j > 0 && grid[i][j - 1] == '.')
         left = matrix[i][j - 1];
+      // add the number of paths above
+      // and to the left of the matrix square
       matrix[i][j] += (above + left);
     }
   }
+  // the total number of paths is the value at the
+  // bottom right square of the grid
   return matrix[grid.size() - 1][grid[0].size() - 1];
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
+  // initialize the grid
   auto grid = parse();
+  // start the timer
   auto start = std::chrono::high_resolution_clock::now();
+  // calulate the number of paths
   int paths = soccer_opponent_aviodance(grid);
+  // stop the clock
   auto stop = std::chrono::high_resolution_clock::now();
+  // calculate duration as microseconds
   auto t = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  // print output to stdout
   std::cout << "Total paths: " << paths << "\n";
   std::cout << "Empirical time: " << t.count() << " us.\n";
   return 0;
