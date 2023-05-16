5fb6698216c1f1ec7c616bbc71649286e5c2b4d7
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-05-16 01:41:05 -0700

    merge soret and test for sorting

--- final_study_code/divide_and_conquer/main.py
+++ final_study_code/divide_and_conquer/main.py
@@ -1,19 +1,52 @@
 def summation(vector):
-    n = len(vector)
-    if n == 0:
-        return n
-    elif n == 1:
-        return vector[0]
-    else:
-        midpoint = int(n/2)
-        left, right = summation(vector[:midpoint]), summation(vector[midpoint:])
-        return left + right
+  n = len(vector)
+  if n == 0:
+    return n
+  elif n == 1:
+    return vector[0]
+  else:
+    midpoint = int(n/2)
+    left = summation(vector[:midpoint])
+    right =  summation(vector[midpoint:])
+    return left + right
 
 
 def summation2(vec):
-    sum = 0
-    for n in vec:
-        sum += n
+  sum = 0
+  for n in vec:
+    sum += n
+  return sum
 
-    return sum
 
+def merge(left, right):
+   if len(left) == 1 and len(right) == 1:
+     n, m = left[0], right[0]
+     if n < m:
+       return [n, m]
+     else:
+       return [m, n]
+   else:
+     output = []
+     while len(left) > 0 and len(right) > 0:
+       n, m = left[0], right[0]
+       if n < m:
+         output.append(left.pop(0))
+       elif m < n:
+         output.append(right.pop(0))
+       else:
+         output.extend([left.pop(0), right.pop(0)])
+     if len(left) > 0:
+       output.extend(left)
+     elif len(right) >0:
+       output.extend(right)
+     return output
+
+def merge_sort(vec):
+   if len(vec) == 1:
+     return vec
+   else:
+     midpoint = len(vec) // 2
+     left = merge_sort(vec[:midpoint])
+     right = merge_sort(vec[midpoint:])
+     sorted_vec = merge(left, right)
+     return sorted_vec
--- final_study_code/divide_and_conquer/test.py
+++ final_study_code/divide_and_conquer/test.py
@@ -1,24 +1,40 @@
 import unittest
-from main import summation
-
+from main import summation, summation2, merge_sort
 
 class TestAlgorithms(unittest.TestCase):
 
-    def test_summation(self):
-        test_inputs = [
-            [1,3,2,4],
-            [1,-1]
-        ]
-        expected_output = [10, 0]
-        for idx, exp in enumerate(expected_output):
-            self.assertEqual(summation(test_inputs[idx]), exp)
+  def test_summation(self):
+    test_inputs = [
+      [1,3,2,4],
+      [1,-1]
+    ]
+    expected_output = [10, 0]
+    for idx, exp in enumerate(expected_output):
+      self.assertEqual(summation(test_inputs[idx]), exp)
 
-    def test_sum(self):
-        test_inputs = [
-            [1,3,2,4],
-            [1,-1]
-        ]
-        expected_output = [10, 0]
-        for idx, exp in enumerate(expected_output):
-            self.assertEqual(summation(test_inputs[idx]), exp)
 
+  def test_sum(self):
+      test_inputs = [
+        [1,3,2,4],
+        [1,-1]
+      ]
+      expected_output = [10, 0]
+      for idx, exp in enumerate(expected_output):
+        self.assertEqual(summation2(test_inputs[idx]), exp)
+
+
+  def test_sort(self):
+    test_inputs = [
+      [1],
+      [1,2],
+      [2,1],
+      [1,32,3,55,1,0,-1,43]
+    ]
+    expected_output = [
+      [1],
+      [1,2],
+      [1,2],
+      [-1,0,1,1,3,32,43,55]
+    ]
+    for idx, exp in enumerate(expected_output):
+      self.assertEqual(merge_sort(test_inputs[idx]), exp)
